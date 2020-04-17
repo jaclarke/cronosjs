6e27cafbcd07dde84084f61769ca42489c192e1b
James Clarke <jaclarke77@gmail.com>
2020-04-17 15:57:24 +0100

    Fix jest config + add more node lts versions to ci config

--- jest.config.js
+++ jest.config.js
@@ -109,12 +109,12 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'tests',
+  // rootDir: null,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "<rootDir>/tests"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
